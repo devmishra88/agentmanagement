097d6df2d5d48bf853865f587dd71c9247467279
devmishra88 <devmishra88@gmail.com>
2023-10-01 22:35:10 +0530

    code formated

--- pama/src/store/AppStore.js
+++ pama/src/store/AppStore.js
@@ -1,12 +1,13 @@
-import {configureStore} from "@reduxjs/toolkit"
+import { configureStore } from "@reduxjs/toolkit";
 
 import CommonReducer from "../slices/CommonSlice";
 
 export const store = configureStore({
-    reducer:{
-        common:CommonReducer
-    },
-    middleware: getDefaultMiddleware => getDefaultMiddleware({
-        serializableCheck:false
-    })
-})
+  reducer: {
+    common: CommonReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
