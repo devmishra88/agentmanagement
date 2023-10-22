524f9b9aba4cfb42869d9c793c59027e56cb0c56
devmishra88 <devmishra88@gmail.com>
2023-10-22 19:57:30 +0530

    slider menu added

--- pama/src/components/index.js
+++ pama/src/components/index.js
@@ -1,7 +1,13 @@
 import Copyright from "./Copyright"
 import Loader from "./Loader"
+import AppHeader from "./AppHeader"
+import AppFooter from "./AppFooter"
+import Mainmenu from "./Mainmenu"
 
 export {
     Copyright,
     Loader,
+    AppHeader,
+    AppFooter,
+    Mainmenu,
 }
